628850c75e7787d88329a3789006459980f876a9
Amanda Mendes <amandamendes@Amandas-MacBook-Air.local>
2022-09-24 20:42:44 -0300

    criando tela de Login

---
+++ projeto-a3-app/src/app/home/home.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    FormsModule
+  ],
+  exports: [HomeComponent]
+})
+export class HomeModule { }
