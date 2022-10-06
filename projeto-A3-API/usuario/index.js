49279ed2e140a0297cec1302832df7b74599e443
NathanVital <nathan.vital@outlook.com>
2022-10-06 20:52:04 -0300

    initial commit - usuarios porta 4000

---
+++ projeto-A3-API/usuario/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const axios = require('axios')
+const app = express()
+
+app.use(express.json())
+
+const usuarios={}
+let organizer = 0
+
+app.post('/usuario',(req, res) =>{
+organizer++
+
+const{texto} = req.body
+usuarios[organizer] = {contador, texto}
+// await axios.post("http://localhost:")
+})
+
+
+
+try {
+    app.listen(4000, () => console.log('Usuário. Porta 4000'))
+} catch (error) {
+    console.log(error)
+}
