cad4e41eae063236bd6502fa428f6c26ba0a93a5
DjalmaOlivei <79875503+DjalmaOlivei@users.noreply.github.com>
2022-10-06 22:06:11 -0300

    Update index.js

--- projeto-A3-API/consulta/index.js
+++ projeto-A3-API/consulta/index.js
@@ -7,22 +7,34 @@ app.use(express.json())
 const consulta =[]
 let organizer = 0
 
-app.post('/consulta',(req, res) =>{
+app.post('/clinica/consulta',(req, res) =>{
 organizer++
 try {
-const{exame} = req.body
-consulta [organizer] = {'id' :organizer, 'exame': exame.descricao , 'dateTime': exame.dateTime}
+const exame = req.body
+consulta [organizer] = {'id' :organizer, 'descricao': exame.descricao , 'dateTime': exame.dateTime, 'paciente': exame.paciente , 'medico': exame.medico , 'status': 'agendado'}
 // await axios.post("http://localhost:")
+res.status(200).send(consulta [organizer])
+
 }
 catch(err){
     res.status(500).send(err)
+    console.log(err)
 }
 })
 
+app.get('/clinica/consulta', (req,res) => {
+    try{
+        res.status(200).send(consulta)
+    }
+    catch(err){
+        res.status(500).send(consulta)
+    }
+
+})
 
 
 try {
-    app.listen(5000, () => console.log('Usuário. Porta 5000'))
+    app.listen(5000, () => console.log('Consulta. Porta 5000'))
 } catch (error) {
     console.log(error)
 }
