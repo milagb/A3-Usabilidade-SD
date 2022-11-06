b370ee0fa95db2f0ae5784c13061320472215a2d
Lucas Lindo <lucaslindo711@hotmail.com>
2022-11-05 21:33:43 -0300

    home: ajuste layout login e cadastro

--- projeto-a3-app/src/app/home/home.module.ts
+++ projeto-a3-app/src/app/home/home.module.ts
@@ -7,6 +7,13 @@ import { LoginComponent } from './login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MensagemModule } from '../componentes/mensagem/mensagem.module';
 import { CadastroUsuarioComponent } from './cadastro-usuario/cadastro-usuario.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input'
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule  } from "@angular/material/form-field";
 
 
 @NgModule({
@@ -20,7 +27,14 @@ import { CadastroUsuarioComponent } from './cadastro-usuario/cadastro-usuario.co
     HomeRoutingModule,
     FormsModule,
     MensagemModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule
   ],
   exports: [HomeComponent]
 })
